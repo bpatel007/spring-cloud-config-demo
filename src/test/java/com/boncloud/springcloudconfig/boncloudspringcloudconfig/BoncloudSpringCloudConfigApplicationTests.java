8645647c5068b89c045502e00ff52054f7779eb3
boncloud <boncloud369@gmail.com>
2018-10-07 09:22:22 +0100

    Initial Commit

---
+++ src/test/java/com/boncloud/springcloudconfig/boncloudspringcloudconfig/BoncloudSpringCloudConfigApplicationTests.java
@@ -0,0 +1,16 @@
+package com.boncloud.springcloudconfig.boncloudspringcloudconfig;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BoncloudSpringCloudConfigApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
